46d9b863ac5162a0dd1aa3c3a1cce28f0ae3c4de
Huzaifasheikh9 <ihuzaifasheikh@gmail.com>
2022-01-24 19:44:07 +0500

    firstm

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f287467b7a8ed23f7b2ee4dbbe08c7e7.js"
+  "/Benji-Challenge-20fff/precache-manifest.05b2ef13eb7de0c456eeb514e39f6975.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Benji-Challenge-20fff/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
